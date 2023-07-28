11b7f5cb5688ef208e65f83feb93f63428e3b3d2
maixuanphong <132797852+maixuanphong@users.noreply.github.com>
2023-07-28 23:58:55 +0700

    28/7/2023 tối

--- src/Components/Layout/DefaultLayout/index.js
+++ src/Components/Layout/DefaultLayout/index.js
@@ -1,12 +1,15 @@
 import Header from '~/Components/Layout/components/Header';
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 import Sidebar from '~/Components/Layout/DefaultLayout/Sidebar';
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header></Header>
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar></Sidebar>
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
